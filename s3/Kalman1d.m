c2107842cf05fa5353f259f566b71f194cbb1989
Joonas Melin <joonas@joonas.melin@tut.fi>
2014-09-25 12:03:53 +0300

    added visualization. Something is not quite working..

--- s3/Kalman1d.m
+++ s3/Kalman1d.m
@@ -1,4 +1,4 @@
-real_pos = -1;
+
 
 moves = [-1,-1,-1,1,1,1,1,-1,-1,-1, -1];
 measurement_var = 1;
@@ -7,6 +7,7 @@ movement_var = 1;
 mean_pos = 2;
 cur_covar = 1;
 
+real_state = mean_pos;
 state = mean_pos; %the actual state of the machine, now only 1d        
 
 
@@ -16,6 +17,9 @@ measure = @(a) (abs(a)+10)+randn()*measurement_var;
 for input = moves
     [state, cur_covar] = kalman_predict(input, state, cur_covar, ...
         movement_var);
+    
+    %moving the real position
+    real_state = real_state+input;
     
     disp(['State after prediction: ', num2str(state), ' std: ', num2str(sqrt(cur_covar))]);
     
@@ -27,6 +31,24 @@ for input = moves
     
     disp(['State after update: ', num2str(state), ' std: ', num2str(sqrt(cur_covar))]);
     
+    %visualization
+    figure(1);
+    hold off;
+    plot(state, 0, 'ob');
+    hold on;
+    plot(real_state, 0, 'xr');
+    
+    %calculating the deviation
+    halfStd = sqrt(cur_covar)/2;
+    plot(state+halfStd, 0.5, 'xg');
+    plot(state-halfStd, 0.5, 'xg');
+    line([state+halfStd, state-halfStd], [0.5, 0.5]);
+    
+    axis([state-2, state+2, -0.2, 0.8]);
+    axis equal;
+    grid on;
+    
+    pause;
     
 end
 

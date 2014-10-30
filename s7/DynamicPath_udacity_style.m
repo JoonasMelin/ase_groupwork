b4ca94f3516cbb05bc458bb4579e0aa7562ea4d8
Tuomas Valimaki <tuomas.s.valimaki@tut.fi>
2014-10-30 21:39:11 +0200

    Added obstacle map and stochastic motion

--- s7/DynamicPath_udacity_style.m
+++ s7/DynamicPath_udacity_style.m
@@ -2,11 +2,13 @@ clear all;
 height = 5;
 width = 6;
 
+grid = rand(height, width) > 0.8;
+
 costMap = zeros(height,width);
 
 valueMap = repmat(999,[height width]);
 
-policyMap = repmat('O',[height width]);
+policyMap = repmat('#',[height width]);
 
 for x = 1:width
     for y = 1:height
@@ -22,6 +24,7 @@ goal = [s(2), s(1)];
 straightMoveCost = 1;
 a = 0.1;
 diagonalMoveCost = 2+a;
+success_prob = 0.7;
 
 %possible Moves for the machine
 delta = [[ -1, 0, straightMoveCost]; % N 
@@ -57,16 +60,29 @@ while changed
                 continue;
             end
 
-            for i = 1:8
-                y2 = y +delta(i,1);
-                x2 = x +delta(i,2);
-                % check for the map limits
-                if x2 >= 1 && x2 <= s(2) && y2 >=1 && y2 <= s(1)
-                    v2 = valueMap(y2,x2)+costMap(y2,x2)+delta(i,3);
+            if grid(y,x) == 0
+                for i = 1:8
+                    v2 = delta(i,3);
+                    for j = 0:1
+                        y2 = y +j*delta(i,1);
+                        x2 = x +j*delta(i,2);
+
+                        if j == 1
+                            p2 = success_prob;
+                        else
+                            p2 = 1- success_prob;
+                        end
+                        % check for the map limits
+                        if x2 >= 1 && x2 <= s(2) && y2 >=1 && y2 <= s(1)
+                            v2 = v2 + p2*(valueMap(y2,x2)+costMap(y2,x2));
+                        else
+                            v2 = v2 + p2*1000; % collision cost
+                        end
+                    end
                     if v2 < valueMap(y,x)
-                        valueMap(y,x)=v2;
-                        policyMap(y,x) = deltaPolicy(i);
-                        changed = true;
+                            valueMap(y,x)=v2;
+                            policyMap(y,x) = deltaPolicy(i);
+                            changed = true;
                     end
                 end
             end
@@ -74,6 +90,8 @@ while changed
     end
 end
 
+disp('Map:');
+grid
 disp('Time matrix:');
 valueMap
 disp('Optimal Path');

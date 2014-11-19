c17d2c08afab9c14a5f3e7e7c359764d59baec5e
Joonas Melin <joonas@joonas.melin@tut.fi>
2014-11-19 17:43:11 +0200

    added the movement models. The turn model does not make sense

--- s9/naoMap.m
+++ s9/naoMap.m
@@ -37,5 +37,20 @@ classdef naoMap < handle
         function estimate_pos(map)
             map.mu = map.omega\map.xi;
         end
+        
+        function [new_a, new_sigma] = turnModel(map, b, cur_a)
+            new_a = b + sqrt((abs(cur_a-b))/(pi/4)); %wtf? this does not make any sense
+            new_sigma = map.sigma_w;
+        end
+        
+        function [new_pos, new_sigma] = movementModel(map, d, cur_pos, cur_a)
+            x = cur_pos(1)+d*cos(cur_a);
+            y = cur_pos(2)+d*sin(cur_a);
+            x_s = cos(cur_a)*map.sigma_l;
+            y_s = sin(cur_a)*map.sigma_l;
+            
+            new_pos = [x y];
+            new_sigma = [x_s y_s];
+        end
     end % methods
 end % classdef
